44ec88356540ae431e19d73d72eaa4e545a834fc
witch-factory <soakdma37@gmail.com>
2021-06-26 13:30:28 +0900

    리뉴얼 part 1, part2 반

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import {Link} from 'gatsby'
+
+const AboutPage=()=>{
+    return (
+        <main>
+            <title>About me </title>
+            <h1>About me</h1>
+            <Link to="/">Back to Home</Link>
+            <p>I created this site.</p>
+        </main>
+    )
+}
+
+export default AboutPage
